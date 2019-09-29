fa3712d502acaa6d4363de29a5c5d11bd48f26af
Matthew M-B <matthewmacraebovell@outlook.com>
2019-09-29 16:08:14 -0400

    added basic seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,25 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 
+3.times do
+    User.create({
+      name: Faker::Name.name,
+      email: Faker::Internet.email,
+      password: "password", #very good password ;)
+    })
+end
 
+5.times do
+    Project.create({
+      title: Faker::Name.name,
+      description: Faker::Lorem.sentence,
+      url:"www.matthewmacraebovell.com",
+      user_id: rand(1..3)
+    })
+end
+
+10.times do
+    Request.create({
+      title: Faker::Name.name,
+      description: Faker::Lorem.sentence,
+      project_id: rand(1..5)
+    })
+end
