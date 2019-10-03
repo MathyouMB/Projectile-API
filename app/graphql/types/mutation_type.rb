6fe42cf9c63316a8b4af26f1d34b17b3d0e65eec
Matthew M-B <matthewmacraebovell@outlook.com>
2019-10-03 13:32:52 -0400

    comment

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,10 +1,15 @@
 module Types
   class MutationType < Types::BaseObject
+    
+    #user mutations
     field :create_user, mutation: Mutations::CreateUser,description: "Create a user."
+    field :signin_user, mutation: Mutations::SignInUser,description: "Signs in mutation."
+    
+    #crud project mutations
     field :create_project, mutation: Mutations::CreateProject,description: "Create a new project"
     field :update_project, mutation: Mutations::UpdateProject,description: "Update a project"
     field :delete_project, mutation: Mutations::DeleteProject,description: "Delete a project"
-    field :signin_user, mutation: Mutations::SignInUser,description: "Signs in mutation."
+    
     
   end
 end
